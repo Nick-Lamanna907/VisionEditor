f857b958edf5dd5d2b2c598d6f4389440e8913c4
Nicholas Lamanna <nllama@Nicholass-Air.modem>
2021-09-14 09:02:30 +1000

    third lesson added, font changes

---
+++ projects/0303ducky.py
@@ -0,0 +1,11 @@
+# Below is some ASCII art of a rubber duck in water
+# Colour it in so that the duck is yellow (or whatever colour you like),
+# and the water is blue!
+# HINT: start off by using print statements to display the duck
+
+from csinsc import *
+
+    __
+  <(o )___
+   ( ._> /
+~~~~`---'~~~~
